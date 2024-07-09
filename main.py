db6f0420092dc21748e381c040d187015a886227
b7sj3o <vitalikfedytnyk@gmail.com>
2024-07-09 23:31:22 +0300

    d

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def main():
     return "<p>Hello, World!</p>"
 
-# if __name__ == "__main__":
-    # app.run(debug=True, h)
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1", port=5000)
+
+
+
+
